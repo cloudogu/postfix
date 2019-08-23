fda9687810fefd7da68a9fef9151db10b29fed0b
Christoph Wolfes <christoph.wolfes@triology.de>
2019-08-23 15:15:32 +0200

    fixes some small shell check issues (bash best practise) #5
    see https://github.com/koalaman/shellcheck/wiki/SC2219
    for further details

--- resources/mask2cidr.sh
+++ resources/mask2cidr.sh
@@ -7,14 +7,14 @@ mask2cidr() {
     IFS=.
     for dec in $1 ; do
         case $dec in
-            255) let nbits+=8;;
-            254) let nbits+=7 ; break ;;
-            252) let nbits+=6 ; break ;;
-            248) let nbits+=5 ; break ;;
-            240) let nbits+=4 ; break ;;
-            224) let nbits+=3 ; break ;;
-            192) let nbits+=2 ; break ;;
-            128) let nbits+=1 ; break ;;
+            255) (( nbits+=8 ));;
+            254) (( nbits+=7 )) ; break ;;
+            252) (( nbits+=6 )) ; break ;;
+            248) (( nbits+=5 )) ; break ;;
+            240) (( nbits+=4 )) ; break ;;
+            224) (( nbits+=3 )) ; break ;;
+            192) (( nbits+=2 )) ; break ;;
+            128) (( nbits+=1 )) ; break ;;
             0);;
             *) echo "Error: $dec is not recognised"; exit 1
         esac
@@ -25,6 +25,6 @@ mask2cidr() {
 ## main ##
 # MASK=255.255.254.0
 MASK=$1
-numbits=$(mask2cidr $MASK)
+numbits=$(mask2cidr "$MASK")
 echo "$numbits"
 exit 0
